60b7e13549ec4dc4689ee4dd0990d7bf76aaa5ac
PedroCandia <petercandiavazquez@hotmail.com>
2024-01-23 11:35:05 -0600

    [BUGFIX] Quitando asincronia en createClient

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,8 +5,8 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://xfkqstoimowntlkgeadm.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhma3FzdG9pbW93bnRsa2dlYWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyMjMzMjQsImV4cCI6MjAxOTc5OTMyNH0.EAOb6ZZhNpScerfzbCt-3jJ53x-tfEtRQnKufYln5qg';
 
-app.get('/createClient', async (req, res) => {
-    const supabase = await createClient(supabaseUrl, supabaseAnonKey);
+app.get('/createClient', (req, res) => {
+    const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
     res.status(200).json(supabase);
 });
